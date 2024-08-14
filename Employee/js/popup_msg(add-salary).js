f1540bc3aca02bd2ee90e5cb429529cf97d27817
siraj-ahmed145 <sirajahmedsa145@gmail.com>
2024-08-15 02:22:51 +0500

    Add files via upload

---
+++ Employee/js/popup_msg(add-salary).js
@@ -0,0 +1,7 @@
+function toggleaddsalary() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupaddsalary = document.getElementById('popupaddsalary');
+    popupaddsalary.classList.toggle('active');
+}
