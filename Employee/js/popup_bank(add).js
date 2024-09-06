eb7bd2026738482836ad37bf444e9cc1165c3e6b
siraj-ahmed145 <sirajahmedsa145@gmail.com>
2024-09-07 00:33:16 +0500

    Add files via upload

---
+++ Employee/js/popup_bank(add).js
@@ -0,0 +1,7 @@
+function toggleAdd() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupadd = document.getElementById('popupadd_bank');
+    popupadd.classList.toggle('active');
+}
---
+++ Employee/js/popup_bank(edit).js
@@ -0,0 +1,7 @@
+function toggleEdit() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupadd = document.getElementById('popupedit_bank');
+    popupadd.classList.toggle('active');
+}
